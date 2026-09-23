5f8cd2308e9640e455778158e25d9c8cded1380a
9KUMa <aoks.11111@gmail.com>
2026-09-23 10:38:18 +0700

    Add camera follow and reload logic
    This commit adds a new camera follow script that smoothly tracks the player and stores a camera offset, plus the Unity meta file for it. It also updates the player controller with reload-related state and a simple reload action that refills ammo and syncs the ammo UI.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -19,6 +20,9 @@ public class PlayerController : MonoBehaviour
     public int magazineSize = 30;
     public int currentAmmo = 30;
     public int reserveAmmo = 5;
+    public float reloadTime = 0.2f;
+    public bool isReloading = false;
+
 
     private void Awake()
     {
@@ -154,9 +158,13 @@ public class PlayerController : MonoBehaviour
 
         if (Mouse.current.rightButton.wasPressedThisFrame)
         {
-            if (reserveAmmo > 0)
+            if (reserveAmmo > 0 )
             {
+             
                 Reload();
+
+
+
             }
         }
     }
@@ -171,11 +179,12 @@ public class PlayerController : MonoBehaviour
         );
         GameManager.Instance.SetAmmo(currentAmmo, magazineSize, reserveAmmo);
     }
+    
 
     private void Reload()
     {
+    
         reserveAmmo--;
-        
         currentAmmo = magazineSize;
         GameManager.Instance.SetAmmo(currentAmmo, magazineSize, reserveAmmo);
     }
